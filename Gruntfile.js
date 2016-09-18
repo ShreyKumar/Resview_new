cb80b0093ce402e49260004be4cacb182a8fd52b
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2016-09-18 02:15:57 -0400

    made uglify work

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,12 @@ module.exports = function(grunt){
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
+    uglify: {
+      build: {
+        src: "js/*.js",
+        dest: "build/main.js"
+      }
+    }
   });
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
