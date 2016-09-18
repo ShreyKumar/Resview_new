033f8c6eec18711de01884eed428c2bcb7c00eda
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2016-09-18 03:20:12 -0400

    added sass task

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,34 @@ module.exports = function(grunt){
         src: "js/*.js",
         dest: "build/main.js"
       }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'build/style.min.css': ['build/css/*.css']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['*.scss'],
+          dest: 'build/css',
+          ext: '.css'
+        }]
+      }
     }
   });
+  /* See answer to SASS here: http://stackoverflow.com/questions/39555122/sass-installation-issues*/
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'sass', 'cssmin']);
 }
